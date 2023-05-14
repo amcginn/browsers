9a16ff545a41fae1bab6399d8ec40f3470ed0fac
Aaron McGinn <aaron.mcginn@cerner.com>
2023-05-13 23:02:17 -0500

    Create insta-no-sponsors.user.js

---
+++ userscripts/insta-no-sponsors.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        insta - no sponsors
+// @namespace   https://github.com/amcginn
+// @match       https://www.instagram.com/
+// @grant       none
+// @version     1.0
+// @author      amcginn
+// @description remove sponsored post content
+// ==/UserScript==
+
+function removeSponsored() {
+  let elements = document.getElementsByTagName('use');
+
+  Array.from(elements).forEach( (element) => {
+    let article = element.closest('article');
+    if (article != null) {
+      article.innerHTML = "<span>&nbsp;&nbsp;Removed sponsored post</span>";
+      console.log('removed element')
+    }
+  })
+}
+
+window.addEventListener('load', function() {
+  setInterval(removeSponsored, 5000);
+});
