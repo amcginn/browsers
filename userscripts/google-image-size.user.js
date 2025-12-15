3cc0cafb979c05f45e58378807a64750ef8cb30e
Aaron McGinn <amcginn_37@hotmail.com>
2025-12-15 01:10:55 -0600

    [US] google-image-size: remove hard coded array retrieval
    Updated version to 1.2.1 and modified image size retrieval logic.

--- userscripts/google-image-size.user.js
+++ userscripts/google-image-size.user.js
@@ -3,10 +3,11 @@
 // @namespace   https://github.com/amcginn/
 // @match       https://www.google.com/search*
 // @grant       none
-// @version     1.2.0
+// @version     1.2.1
 // @author      amcginn
 // @license     MIT
 // @description Add image sizes to Google Image search results.
+// @changelog   1.2.1 - Use dynamic array retrieval
 // @changelog   1.2.0 - Refactor from Google changes: pull from window js objects
 // @changelog   1.1.0 - Use MutationObserver, debouncing, skip Google's own dimensions, improved positioning
 // @changelog   1.0 - Initial release
@@ -22,6 +23,8 @@ function createSizeEl(width, height) {
 }
 
 function showImgSizes() {
+  if (!window.W_jd) return;
+
   let searchImageResults = document.querySelectorAll('h3:has(> a img):not(.image-size)');
 
   for (const result of searchImageResults) {
@@ -29,12 +32,13 @@ function showImgSizes() {
       let container = result.closest('[data-ved]')
       let imgId = container.getAttribute('jsdata').split(';').pop()
       let arr = window.W_jd[imgId];
-      let syms = Object.getOwnPropertySymbols(arr);
-      let realArr = arr[syms[0]].Ar
+      let sym = Object.getOwnPropertySymbols(arr)[0]
+      let realArr = sym && Object.values(arr[sym])[0] // array of all image results
 
-      let fullImg = realArr[1].Ar[3]
-      let height = fullImg[1]
-      let width = fullImg[2]
+      let fullImgDetails = realArr && Object.values(realArr[1])[0] // includes links, preview/original image, background color, etc.
+      let fullImg = fullImgDetails && fullImgDetails[3] // index 2 is thumbnail, index 3 is original
+      let height = fullImg && fullImg[1] // 0: image link, 1: height, 2: width
+      let width = fullImg && fullImg[2]
 
       if (!width || !height) continue;
 
@@ -57,8 +61,6 @@ function debouncedShowImgSizes() {
 }
 
 window.addEventListener('load', () => {
-  if (!window.W_jd) return;
-
   try {
     const observer = new MutationObserver(debouncedShowImgSizes);
     observer.observe(document.body, { childList: true, subtree: true });
